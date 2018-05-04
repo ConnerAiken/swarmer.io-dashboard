c445b01bf070eaaf5b11fc39c5740eca2bf73c5b
conneraiken <conner@fittedtech.com>
2018-05-04 09:57:24 -0700

    Add feature section

--- src/components/home/controller.jsx
+++ src/components/home/controller.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './styles.scss';
 import bg from "./../../assets/images/swarm.jpg";
+import panda from "./../../assets/images/panda.png";
 
 export default class Home extends React.Component { 
     constructor(props) {
@@ -15,8 +16,21 @@ export default class Home extends React.Component {
                 <hr/>
                 <h4>Functional Load Testing</h4>
             </div> 
-            <div> 
-                <a href="http://swarmer.io/dash">Try It Out</a>
+            <div>  
+                <div> 
+                    <h4>Well, what are you waiting for?</h4>
+                    <ul>
+                        <li>Load test your web application from the cloud</li>
+                        <li>Simulate actual user behavior</li>
+                        <li>Detailed reports of user experience</li>
+                        <li>Import existing or design new end to end tests</li>
+                    </ul>
+                    <a href="/dash" class="button">Try It Out</a>
+                    <p><small>* Compatible with Webdriverio and Nightwatch</small></p>
+                </div>
+                <div>
+                    <img src={panda}/>
+                </div>
             </div> 
         </div>
       );
